ed8536b229c426e5b3dda0e517868a9efe36bcd2
JiangLuyu <32221264+Jiangluyu@users.noreply.github.com>
2019-08-02 23:14:19 +0800

    Add files via upload

--- login/forms.py
+++ login/forms.py
@@ -3,8 +3,8 @@ from captcha.fields import CaptchaField
 
 
 class UserForm(forms.Form):
-    username = forms.CharField(label="用户名", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="密码", max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     captcha = CaptchaField(label="验证码")
 
 
@@ -15,8 +15,47 @@ class RegisterForm(forms.Form):
     )
     username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="确认密码", max_length=256,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    sex = forms.ChoiceField(label='性别', choices=gender)
-    captcha = CaptchaField(label='验证码')
+    sex = forms.ChoiceField(label="性别", choices=gender)
+    captcha = CaptchaField(label="验证码")
+    question_1 = forms.CharField(label="密保问题01", max_length=255,
+                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
+    answer_1 = forms.CharField(label="问题答案01", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    question_2 = forms.CharField(label="密保问题02", max_length=255,
+                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
+    answer_2 = forms.CharField(label="问题答案02", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    question_3 = forms.CharField(label="密保问题03", max_length=255,
+                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
+    answer_3 = forms.CharField(label="问题答案03", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+
+class ForgetForm(forms.Form):
+    email = forms.EmailField(label="邮箱", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    captcha = CaptchaField(label="验证码")
+
+
+class RetrieveForm(forms.Form):
+    answer_1 = forms.CharField(label="问题答案01", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    answer_2 = forms.CharField(label="问题答案02", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    answer_3 = forms.CharField(label="问题答案03", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+
+class ResetForm(forms.Form):
+    new_password1 = forms.CharField(label="请输入新密码", required=True, min_length=6,
+                                    error_messages={'required': '密码不能为空.', 'min_length': "至少6位"},
+                                    widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    new_password2 = forms.CharField(label="再次输入新密码", required=True, min_length=6,
+                                    error_messages={'required': '密码不能为空.', 'min_length': "至少6位"},
+                                    widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class GradeForm(forms.Form):
+    new_class_id = forms.CharField(label="课程号", required=True, error_messages={'required': '课程号不能为空！'},
+                                   widget=forms.TextInput(attrs={'class': 'form-control'}))
+    new_class_name = forms.CharField(label="课程名", required=True, error_messages={'required': '课程名不能为空！'},
+                                     widget=forms.TextInput(attrs={'class': 'form-control'}))
+    new_grade = forms.CharField(label="成绩", required=True, error_messages={'required': '成绩不能为空！'},
+                                widget=forms.TextInput(attrs={'class': 'form-control'}))
 
--- login/models.py
+++ login/models.py
@@ -1,72 +1,20 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
-class Grades(models.Model):
-    student_id = models.CharField(max_length=255)
-    class_id = models.CharField(max_length=255)
-    class_name = models.CharField(max_length=255)
-    grade = models.CharField(max_length=255)
-    grade_point = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'grades'
+# Create your models here.
+# login/models.py
 
 
-class Logs(models.Model):
-    name = models.CharField(max_length=128, blank=True, null=True)
-    operation = models.CharField(max_length=255, blank=True, null=True)
-    time = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'logs'
-
-
-class RetrieveQuestions(models.Model):
-    name = models.CharField(primary_key=True, max_length=128)
-    question_1 = models.CharField(max_length=255, blank=True, null=True)
-    question_2 = models.CharField(max_length=255, blank=True, null=True)
-    question_3 = models.CharField(max_length=255, blank=True, null=True)
-    answer_1 = models.CharField(max_length=255, blank=True, null=True)
-    answer_2 = models.CharField(max_length=255, blank=True, null=True)
-    answer_3 = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'retrieve_questions'
-
-
-class Students(models.Model):
-    student_id = models.CharField(primary_key=True, max_length=255)
-    student_name = models.CharField(max_length=128)
-    gender = models.IntegerField()
-    age = models.IntegerField()
-    birth = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'students'
-
-
-class Users(models.Model):
+class User(models.Model):
     gender = (
         ('male', '男'),
         ('female', '女'),
     )
-
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128,unique=True)
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
     sex = models.CharField(max_length=32, choices=gender, default='男')
     c_time = models.DateTimeField(auto_now_add=True)
+    c_kind = models.CharField(max_length=128, default='普通用户')
 
     def __str__(self):
         return self.name
@@ -75,4 +23,34 @@ class Users(models.Model):
         ordering = ['c_time']
         verbose_name = '用户'
         verbose_name_plural = '用户'
+
+
+class Grade(models.Model):
+    id = models.AutoField(primary_key=True, max_length=255)
+    student_id = models.CharField(max_length=255)
+    class_id = models.CharField(max_length=255)
+    class_name = models.CharField(max_length=255)
+    grade = models.CharField(max_length=255)
+    grade_point = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
+
+
+class RetrieveQuestion(models.Model):
+    name = models.CharField(primary_key=True, max_length=128)
+    question_1 = models.CharField(max_length=255, blank=True)
+    question_2 = models.CharField(max_length=255, blank=True)
+    question_3 = models.CharField(max_length=255, blank=True)
+    answer_1 = models.CharField(max_length=255, blank=True)
+    answer_2 = models.CharField(max_length=255, blank=True)
+    answer_3 = models.CharField(max_length=255, blank=True)
+
+
+class Student(models.Model):
+    student_id = models.CharField(primary_key=True, max_length=255)
+    student_name = models.CharField(max_length=128)
+    gender = models.IntegerField()
+    age = models.IntegerField()
+    birth = models.DateTimeField(max_length=6)
+
+
+
 
